d54a98af9cd10e943a6176e5853c5ecabbc195ca
Long Doan Vu (FICO) <LongDV1@SOU.TPB.COM>
2019-11-05 18:19:23 +0700

    update

--- tpf-service-webportal/client/src/views/Admin/clients.vue
+++ tpf-service-webportal/client/src/views/Admin/clients.vue
@@ -2,8 +2,9 @@
   <tpf-table
     project="dataentry"
     :header="headers"
-    :pagination="true"
+    :pagination="false"
     :index="true"
+    :tagColor="tagColor"
   ></tpf-table>
 </template>
 
@@ -18,10 +19,20 @@ export default {
       headers: [
         { key: "createdAt", title: "createdAt", align: "center", header_align: "center", type: 'DateTime' },
         { key: "appId", title: "appId", align: "center", header_align: "center" },
-        { key: "status", title: "status", align: "center", header_align: "center", type: 'Label' },
+        { key: "status", title: "status", align: "left", header_align: "center", type: 'Label' },
         { key: "fullName", title: "fullName", align: "center", header_align: "center" },
         { key: "nationalId", title: "nationalId", align: "center", header_align: "center" }
-      ]
+      ],
+      tagColor: {
+        PROCESSING_FAIL: "#303133",
+        PROCESSING_PASS: "success",
+        OK: "success",
+        PROCESSING: "warning",
+        APPROVED_FINNONE: "infor",
+        AUTO_QL_FAIL: "error",
+        FAIL: "error",
+        COMPLETED: "success",
+      }
     }
   },
   created() {
@@ -32,3 +43,5 @@ export default {
   }
 }
 </script>
+
+
