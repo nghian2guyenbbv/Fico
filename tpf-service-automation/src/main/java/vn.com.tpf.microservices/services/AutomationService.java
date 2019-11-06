4ffb747a67ba5c63be9b6d86292f514e67b7f0b7
nghia hiep <nghiahiep512@gmail.com>
2019-11-06 11:55:27 +0700

    update more acc momo automation

--- tpf-service-automation/src/main/java/vn.com.tpf.microservices/services/AutomationService.java
+++ tpf-service-automation/src/main/java/vn.com.tpf.microservices/services/AutomationService.java
@@ -54,10 +54,10 @@ public class AutomationService {
 
 	final static List<LoginDTO> momoAccounts= Arrays.asList(
             LoginDTO.builder().userName("momo_auto1").password("Hcm@12345").build()
-//            LoginDTO.builder().userName("momo_auto2").password("Hcm@12345").build(),
-//            LoginDTO.builder().userName("momo_auto3").password("Hcm@12345").build(),
-//            LoginDTO.builder().userName("momo_auto4").password("Hcm@12345").build(),
-//            LoginDTO.builder().userName("momo_auto5").password("Hcm@12345").build()
+            LoginDTO.builder().userName("momo_auto2").password("Hcm@12345").build(),
+            LoginDTO.builder().userName("momo_auto3").password("Hcm@12345").build(),
+            LoginDTO.builder().userName("momo_auto4").password("Hcm@12345").build(),
+            LoginDTO.builder().userName("momo_auto5").password("Hcm@12345").build()
 	);
 	final static Queue<LoginDTO> momo_loginDTOQueue = new LinkedBlockingQueue<>(momoAccounts);
 
