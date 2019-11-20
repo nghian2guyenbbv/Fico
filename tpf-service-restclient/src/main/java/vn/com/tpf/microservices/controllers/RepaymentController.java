4bf61cb359861969670d9c41510916ceae4652a7
nghia hiep <nghiahiep512@gmail.com>
2019-11-20 14:41:47 +0700

    update button syncdata f1 repayment

--- tpf-service-restclient/src/main/java/vn/com/tpf/microservices/controllers/RepaymentController.java
+++ tpf-service-restclient/src/main/java/vn/com/tpf/microservices/controllers/RepaymentController.java
@@ -117,6 +117,20 @@ public class RepaymentController {
 				.header("x-pagination-total", response.path("total").asText("0")).body(response.path("data"));
 	}
 
+	@PostMapping("/v1/repayment/syncData")
+	@PreAuthorize("#oauth2.hasAnyScope('tpf-service-repayment','tpf-service-root')")
+	public ResponseEntity<?> syncData(@RequestHeader("Authorization") String token, @RequestBody JsonNode body)
+			throws Exception {
+		Map<String, Object> request = new HashMap<>();
+		request.put("func", "syncData");
+		request.put("token", token);
+		request.put("body", body);
+
+		JsonNode response = rabbitMQService.sendAndReceive("tpf-service-repayment", request);
+		return ResponseEntity.status(response.path("status").asInt(500))
+				.header("x-pagination-total", response.path("total").asText("0")).body(response.path("data"));
+	}
+
 //	@GetMapping("/repayment/customers_pay")
 //	@PreAuthorize("#oauth2.hasAnyScope('tpf-service-repayment')")
 //	public ResponseEntity<?> reads2(@RequestHeader("Authorization") String token, @RequestBody JsonNode body)
