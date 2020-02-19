39affdfccec900a6f706742349fee62f4ad63a77
ducchuyen <chuyen.nd.9711@gmail.com>
2020-02-19 11:44:43 +0700

    UAT add a information Fpt record

--- tpf-service-fpt/src/main/java/vn/com/tpf/microservices/models/Fpt.java
+++ tpf-service-fpt/src/main/java/vn/com/tpf/microservices/models/Fpt.java
@@ -15,7 +15,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
 
 @Data
-@Document
+@Document(collection = "fpt")
 @JsonInclude(Include.NON_NULL)
 public class Fpt {
 
--- tpf-service-fpt/src/main/java/vn/com/tpf/microservices/models/Supplement.java
+++ tpf-service-fpt/src/main/java/vn/com/tpf/microservices/models/Supplement.java
@@ -1,5 +1,10 @@
 package vn.com.tpf.microservices.models;
 
+import java.util.Date;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
@@ -13,5 +18,28 @@ public class Supplement {
 	private String commentTpf;
 	private String commentFpt;
 	private boolean isPending;
+	private String type;
+	@CreatedDate
+	private Date createdAt;
+	@LastModifiedDate
+	private Date updatedAt;
+
+	public void SupplementFpt(String code, String commentFpt, String commentTpf, String type, Date updatedAt,Date createdAt) {
+
+		this.code = code;
+		this.commentFpt = commentFpt;
+		this.commentTpf = commentTpf;
+		this.updatedAt = updatedAt;
+		this.isPending = true;
+		this.createdAt = createdAt;
+	}
+
+	public void SupplemenTPBank(String code, String commentTpf, String type, Date createdAt) {
+		this.code = code;
+		this.commentTpf = commentTpf;
+		this.isPending = false;
+		this.type = type;
+		this.createdAt = createdAt;
+	}
 
 }
