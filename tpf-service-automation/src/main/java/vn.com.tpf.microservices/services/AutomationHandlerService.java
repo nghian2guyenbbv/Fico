6d299596ad052337e340cc9d7eb1c71981639fa2
AnhLTD <ikuysle@gmail.com>
2020-03-19 08:59:17 +0700

    Update code SGB

--- tpf-service-automation/src/main/java/vn.com.tpf.microservices/services/AutomationHandlerService.java
+++ tpf-service-automation/src/main/java/vn.com.tpf.microservices/services/AutomationHandlerService.java
@@ -65,8 +65,8 @@ public class AutomationHandlerService {
     @Value("${spring.url.downloadFile}")
     private String downdloadFileURL;
 
-//    @Value("${spring.url.rabbitIdRes}")
-//    private String rabbitIdRes;
+    @Value("${spring.url.rabbitIdRes}")
+    private String rabbitIdRes;
 
     private LoginDTO pollAccountFromQueue_OLD(Queue<LoginDTO> accounts, String project) throws Exception {
         LoginDTO accountDTO = null;
@@ -1047,11 +1047,7 @@ public class AutomationHandlerService {
 //                Map.of("func", func, "token",
 //                        String.format("Bearer %s", rabbitMQService.getToken().path("access_token").asText()),"body", application));
 
-//        JsonNode jsonNode = rabbitMQService.sendAndReceive(rabbitIdRes,
-//                Map.of("func", func, "body", application));
-//        System.out.println("rabit:=>" + jsonNode.toString());
-
-        JsonNode jsonNode = rabbitMQService.sendAndReceive("tpf-service-dataentry",
+        JsonNode jsonNode = rabbitMQService.sendAndReceive(rabbitIdRes,
                 Map.of("func", func, "body", application));
         System.out.println("rabit:=>" + jsonNode.toString());
 
