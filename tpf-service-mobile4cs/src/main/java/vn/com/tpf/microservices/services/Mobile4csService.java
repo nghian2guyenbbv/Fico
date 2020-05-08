d7ace013eca1f00e6b1d2281c52464a28609e9e1
HieuTP1 <HieuTP1@tpb.com.vn>
2020-05-08 14:28:37 +0700

    add function getProfile

--- tpf-service-mobile4cs/src/main/java/vn/com/tpf/microservices/services/Mobile4csService.java
+++ tpf-service-mobile4cs/src/main/java/vn/com/tpf/microservices/services/Mobile4csService.java
@@ -9,17 +9,15 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import vn.com.tpf.microservices.commons.Response;
 import vn.com.tpf.microservices.commons.ResultData;
 import vn.com.tpf.microservices.dao.Mobiles4csDAO;
+import vn.com.tpf.microservices.models.ResponseModel;
 
 import java.sql.Clob;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class Mobile4csService {
@@ -29,75 +27,66 @@ public class Mobile4csService {
     private ObjectMapper mapper;
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    @Autowired
     private RabbitMQService rabbitMQService;
 
-    private JsonNode response(int status, JsonNode data) {
-        ObjectNode response = mapper.createObjectNode();
-        response.put("status", status).set(Response.DATA, data);
-        return response;
-    }
+    @Autowired
+    private Mobiles4csDAO mobiles4csDAO;
 
     public JsonNode getHadLoan(JsonNode request) {
-        ObjectNode fileRes = mapper.createObjectNode();
+        ResponseModel responseModel = new ResponseModel();
         String logStr = "";
         logStr += "Request Data : " + request;
-        fileRes.put(Response.REQUEST_ID, request.path("body").path(Response.REQUEST_ID).textValue());
-        fileRes.put(Response.REFERENCE_ID, UUID.randomUUID().toString());
+        responseModel.setRequest_id(request.path("body").path(Response.REQUEST_ID).textValue());
+        responseModel.setReference_id(UUID.randomUUID().toString());
 
         try {
-            Mobiles4csDAO dao = new Mobiles4csDAO();
             Map<String, String> sqlParam = new HashMap<>();
             sqlParam.put("P_CMND", request.path("body").path(Response.DATA).path("idCardNumber").textValue());
             sqlParam.put("P_PHONE", request.path("body").path(Response.DATA).path("phoneNumber").textValue());
 
-            String sum = dao.getStringData(jdbcTemplate, "FN_PRECHECK_MOBILE4CS", sqlParam);
+            String sum = mobiles4csDAO.getStringData("FN_PRECHECK_MOBILE4CS", sqlParam);
             JSONObject dataSql = new JSONObject(sum);
 
-            fileRes.put(Response.RESULT_CODE, ResultData.SUCCESS.getResultCode());
-            fileRes.put(Response.RESULT_MESSAGE, dataSql.getString("description"));
-            String[] countLoan = dataSql.getString("description").split("\\s+");
-            if(!countLoan[3].equals("có")){
-                fileRes.put("valid", Integer.parseInt(countLoan[3]));
+            responseModel.setResult_code(ResultData.SUCCESS.getResultCode());
+            responseModel.setResult_message(dataSql.getString("description"));
+            if (1 == dataSql.getInt("result")) {
+                responseModel.setValid(1);
             }
 
             logStr += "SQL Data : " + sum;
-            logStr += "Response: " + fileRes;
+            logStr += "Response: " + responseModel.toString();
         } catch (Exception e) {
             log.info("Error: " + e);
-            fileRes.put(Response.RESULT_CODE, ResultData.OTHER_ERROR.getResultCode());
-            fileRes.put(Response.RESULT_MESSAGE, ResultData.OTHER_ERROR.getResultMessage());
+            responseModel.setResult_code(ResultData.OTHER_ERROR.getResultCode());
+            responseModel.setResult_message(ResultData.OTHER_ERROR.getResultMessage());
         } finally {
             log.info(logStr);
         }
-        return response(200, fileRes);
+        return responseModel.getResponseModel(mapper);
     }
 
     public JsonNode getQuickInquiry(JsonNode request) throws SQLException {
-        ObjectNode fileRes = mapper.createObjectNode();
+        ResponseModel responseModel = new ResponseModel();
         String logStr = "";
         logStr += "Request Data : " + request;
-        fileRes.put(Response.REQUEST_ID, request.path("body").path(Response.REQUEST_ID).textValue());
-        fileRes.put(Response.REFERENCE_ID, UUID.randomUUID().toString());
+        responseModel.setRequest_id(request.path("body").path(Response.REQUEST_ID).textValue());
+        responseModel.setReference_id(UUID.randomUUID().toString());
 
         try {
-            Mobiles4csDAO dao = new Mobiles4csDAO();
             Map<String, String> sqlParam = new HashMap<>();
             sqlParam.put("P_CMND", request.path("body").path(Response.DATA).path("idCardNumber").textValue());
             sqlParam.put("P_PHONE", request.path("body").path(Response.DATA).path("phoneNumber").textValue());
 
-            Clob clob = dao.getClobData(jdbcTemplate, "FN_QUICK_INQUIRY_MOBILE4CS", sqlParam);
-            String dataClob = dao.handleClob(clob);
+            Clob clob = mobiles4csDAO.getClobData("FN_QUICK_INQUIRY_MOBILE4CS", sqlParam);
+            String dataClob = mobiles4csDAO.handleClob(clob);
             JSONArray parseDataClob = new JSONArray(dataClob);
 
-            fileRes.put(Response.RESULT_CODE, ResultData.SUCCESS.getResultCode());
+            responseModel.setResult_code(ResultData.SUCCESS.getResultCode());
 
             if (parseDataClob.isEmpty()) {
-                fileRes.put(Response.RESULT_MESSAGE, "Không có khoảng vay nào");
+                responseModel.setResult_message("Không có khoảng vay nào");
             } else {
-                fileRes.put(Response.RESULT_MESSAGE, "Có " + parseDataClob.length() + " khoảng vay");
+                responseModel.setResult_message("Có " + parseDataClob.length() + " khoảng vay");
                 ArrayNode dataArrayNode = mapper.createArrayNode();
                 for (Object o : parseDataClob) {
                     if (o instanceof JSONObject) {
@@ -113,39 +102,39 @@ public class Mobile4csService {
                         dataArrayNode.add(data);
                     }
                 }
-                fileRes.set(Response.DATA, dataArrayNode);
+                responseModel.setData(dataArrayNode);
             }
 
             logStr += "SQL Data : " + clob;
-            logStr += "Response: " + fileRes;
+            logStr += "Response: " + responseModel.toString();
         } catch (Exception e) {
             log.info("Error: " + e);
-            fileRes.put(Response.RESULT_CODE, ResultData.OTHER_ERROR.getResultCode());
-            fileRes.put(Response.RESULT_MESSAGE, ResultData.OTHER_ERROR.getResultMessage());
+            responseModel.setResult_code(ResultData.OTHER_ERROR.getResultCode());
+            responseModel.setResult_message(ResultData.OTHER_ERROR.getResultMessage());
         } finally {
             log.info(logStr);
         }
 
-        return response(200, fileRes);
+        return responseModel.getResponseModel(mapper);
     }
 
     public JsonNode getDetailInquiry(JsonNode request) throws SQLException {
-        ObjectNode fileRes = mapper.createObjectNode();
+        ResponseModel responseModel = new ResponseModel();
         String logStr = "";
         logStr += "Request Data : " + request;
-        fileRes.put(Response.REQUEST_ID, request.path("body").path(Response.REQUEST_ID).textValue());
-        fileRes.put(Response.REFERENCE_ID, UUID.randomUUID().toString());
+        responseModel.setRequest_id(request.path("body").path(Response.REQUEST_ID).textValue());
+        responseModel.setReference_id(UUID.randomUUID().toString());
 
         try {
-            Mobiles4csDAO dao = new Mobiles4csDAO();
+
             Map<String, String> sqlParam = new HashMap<>();
             sqlParam.put("P_LOAN_ACCOUNT", request.path("body").path(Response.DATA).path("loanAccountNumber").textValue());
 
-            String sum = dao.getStringData(jdbcTemplate, "FN_DETAIL_INQUIRY_MOBILE4CS", sqlParam);
+            String sum = mobiles4csDAO.getStringData("FN_DETAIL_INQUIRY_MOBILE4CS", sqlParam);
             JSONObject dataSql = new JSONObject(sum);
             ObjectNode data = mapper.createObjectNode();
-            fileRes.put(Response.RESULT_CODE, ResultData.SUCCESS.getResultCode());
-            fileRes.put(Response.RESULT_MESSAGE, ResultData.SUCCESS.getResultMessage());
+            responseModel.setResult_code(ResultData.SUCCESS.getResultCode());
+            responseModel.setResult_message(ResultData.SUCCESS.getResultMessage());
             if (!dataSql.isEmpty()) {
                 data.put("loanAccountNumber  ", dataSql.getString("loanId"));
                 data.put("productSchemaName", dataSql.getString("prodName"));
@@ -175,54 +164,91 @@ public class Mobile4csService {
                 }
                 data.put("schedule", dataArrayNode);
 
-                fileRes.set(Response.DATA, data);
+                responseModel.setData(data);
             }
 
             logStr += "SQL Data : " + sum;
-            logStr += "Response: " + fileRes;
+            logStr += "Response: " + responseModel.toString();
         } catch (Exception e) {
             log.info("Error: " + e);
-            fileRes.put(Response.RESULT_CODE, ResultData.OTHER_ERROR.getResultCode());
-            fileRes.put(Response.RESULT_MESSAGE, ResultData.OTHER_ERROR.getResultMessage());
+            responseModel.setResult_code(ResultData.OTHER_ERROR.getResultCode());
+            responseModel.setResult_message(ResultData.OTHER_ERROR.getResultMessage());
         } finally {
             log.info(logStr);
         }
-        return response(200, fileRes);
+        return responseModel.getResponseModel(mapper);
     }
 
     public JsonNode getProfile(JsonNode request) throws SQLException {
-        ObjectNode fileRes = mapper.createObjectNode();
-        fileRes.put(Response.REQUEST_ID, request.path("body").path(Response.REQUEST_ID).textValue());
-        fileRes.put(Response.REFERENCE_ID, UUID.randomUUID().toString());
-        fileRes.put(Response.RESULT_CODE, ResultData.SUCCESS.getResultCode());
-        fileRes.put(Response.RESULT_MESSAGE, ResultData.SUCCESS.getResultMessage());
-        ObjectNode data = mapper.createObjectNode();
-        data.put(" softCopyUrl", " softCopyUrl");
-        fileRes.set(Response.DATA, data);
-        return response(200, fileRes);
+        ResponseModel responseModel = new ResponseModel();
+        String logStr = "";
+        logStr += "Request Data : " + request;
+        responseModel.setRequest_id(request.path("body").path(Response.REQUEST_ID).textValue());
+        responseModel.setReference_id(UUID.randomUUID().toString());
+        try {
+            Map<String, String> sqlParam = new HashMap<>();
+            sqlParam.put("P_CMND", request.path("body").path(Response.DATA).path("idCardNumber").textValue());
+            sqlParam.put("P_PHONE", request.path("body").path(Response.DATA).path("phoneNumber").textValue());
+
+            String sum = mobiles4csDAO.getStringData("FN_USER_PROFILE_MOBILE4CS", sqlParam);
+            JSONObject dataSql = new JSONObject(sum);
+            responseModel.setResult_code(ResultData.SUCCESS.getResultCode());
+            responseModel.setResult_message(ResultData.SUCCESS.getResultMessage());
+            ObjectNode data = mapper.createObjectNode();
+            data.put(" fullName", dataSql.getString("fullName"));
+            data.put(" dateOfBirth", dataSql.getString("DOB"));
+            data.put(" phoneNumber", getStringFromJsonArray(dataSql.getJSONArray("phoneNo")));
+            data.put(" idCardNumber", dataSql.getString("idCard"));
+            data.put(" currentAddress", dataSql.getString("curAdd"));
+            data.put(" permanentAddress", dataSql.getString("perAdd"));
+            data.put(" companyName", dataSql.getString("comName").toString());
+            data.put(" workingAddress", dataSql.getString("perAdd"));
+            data.put(" workingPhoneNumber", "telePhone: " + getStringFromJsonArray(dataSql.getJSONObject("comPhone").getJSONArray("telePhone")) + "mobiPhone: " + getStringFromJsonArray(dataSql.getJSONObject("comPhone").getJSONArray("mobiPhone")));
+            data.put(" referencePhoneNumber", getStringFromJsonArray(dataSql.getJSONArray("refPhone")));
+            data.put(" emailAddress", dataSql.getString("email"));
+            data.put(" zaloAddress", dataSql.getString("zalo"));
+
+            responseModel.setData(data);
+        } catch (Exception e) {
+            log.info("Error: " + e);
+            responseModel.setResult_code(ResultData.OTHER_ERROR.getResultCode());
+            responseModel.setResult_message(ResultData.OTHER_ERROR.getResultMessage());
+        } finally {
+            log.info(logStr);
+        }
+        return responseModel.getResponseModel(mapper);
+    }
+
+    private String getStringFromJsonArray(JSONArray arr) {
+        JSONArray jsonArray = arr;
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            list.add(jsonArray.getString(i));
+        }
+        return list.toString().replace("[", "").replace("]", "");
     }
 
     public JsonNode sendSms(JsonNode request) throws Exception {
-        ObjectNode fileRes = mapper.createObjectNode();
+        ResponseModel responseModel = new ResponseModel();
         Map<String, Object> requestRabbit = new HashMap<>();
-        try{
+        try {
             requestRabbit.put("func", "sendSms");
-            requestRabbit.put("body",request);
+            requestRabbit.put("body", request);
             JsonNode response = rabbitMQService.sendAndReceive("tpf-service-sms", requestRabbit);
             String resul_code = response.get("resultCode").asText();
-            if(resul_code.equals("0")){
-                fileRes.put(Response.RESULT_CODE, ResultData.SUCCESS.getResultCode());
-                fileRes.put(Response.RESULT_MESSAGE, ResultData.SUCCESS.getResultMessage());
+            if (resul_code.equals("0")) {
+                responseModel.setResult_code(ResultData.SUCCESS.getResultCode());
+                responseModel.setResult_message(ResultData.SUCCESS.getResultMessage());
                 log.info("Data: " + response.path("data").asText());
-            }else{
-                fileRes.put(Response.RESULT_CODE, ResultData.FAIL.getResultCode());
-                fileRes.put(Response.RESULT_MESSAGE, ResultData.FAIL.getResultMessage());
+            } else {
+                responseModel.setResult_code(ResultData.FAIL.getResultCode());
+                responseModel.setResult_message(ResultData.FAIL.getResultMessage());
                 log.info("Message: " + response.path("message").asText());
             }
         } catch (Exception e) {
             log.info("Error: " + e);
         }
 
-        return response(200, fileRes);
+        return responseModel.getResponseModel(mapper);
     }
 }
