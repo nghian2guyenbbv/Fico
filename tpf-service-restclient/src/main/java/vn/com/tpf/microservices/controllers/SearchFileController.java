ff1dd107df10e90ec6ca23b65161d6142f6732bb
nghia hiep <nghiahiep512@gmail.com>
2020-06-05 14:20:43 +0700

    update code

--- tpf-service-restclient/src/main/java/vn/com/tpf/microservices/controllers/SearchFileController.java
+++ tpf-service-restclient/src/main/java/vn/com/tpf/microservices/controllers/SearchFileController.java
@@ -19,13 +19,14 @@ public class SearchFileController {
 	private RabbitMQService rabbitMQService;
 
 	@PostMapping("/search-file-by-keyword")
+	@PreAuthorize("#oauth2.hasAnyScope('tpf-service-root')")
 	public ResponseEntity<?> create( @RequestBody JsonNode body)
 			throws Exception {
 		Map<String, Object> request = new HashMap<>();
 		request.put("func", "getListPathFileByKeyword");
 		request.put("body", body);
 
-		JsonNode response = rabbitMQService.sendAndReceive("tpf-service-search-folder", request);
+		JsonNode response = rabbitMQService.sendAndReceive("tpf-service-search-file", request);
 		return ResponseEntity.status(response.path("status").asInt(500))
 				.header("x-pagination-total", response.path("total").asText("0")).body(response.path("data"));
 	}
