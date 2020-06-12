bd8fcfa0784d8812baae0f50c8f3d0470141409b
trung_lai <chitrung7190@gmail.com>
2020-06-12 14:31:16 +0700

    remove unsued config

--- tpf-service-eureka/src/main/java/vn/com/tpf/microservices/Application.java
+++ tpf-service-eureka/src/main/java/vn/com/tpf/microservices/Application.java
@@ -1,13 +1,8 @@
 package vn.com.tpf.microservices;
 
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -15,19 +10,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Value("${spring.rabbitmq.app-id}")
-	private String appId;
-
-	@Bean
-	public Queue queue() {
-		return new Queue(appId, true, false, false);
-	}
-
-	@RabbitListener(queues = "${spring.rabbitmq.app-id}")
-	public Message onMessage() {
-		return null;
 	}
 
 }
