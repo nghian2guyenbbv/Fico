2e44bb40855f76eb8aff37e0c52ab18cf0896ead
THUC_VTVTPF <trungthuc.it.spk02@gmail.com>
2020-06-22 14:12:24 +0700

    Clone project CRM from Mobility service

---
+++ tpf-service-crm/src/main/java/vn/com/tpf/microservices/models/CrmFinnOneFiled.java
@@ -0,0 +1,43 @@
+package vn.com.tpf.microservices.models;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document
+public class CrmFinnOneFiled {
+
+	@Id
+	private String id;
+
+	private Object noOfAttempts;
+	
+	private Object verificationAgent;
+	
+	private Object resonDecisionFic;
+	
+	private Object resultDecisionFiv;
+	
+	private Object result2ndHomeVisit;
+	
+	private Object resultOfficeVisit;
+	
+	private Object resultHomeVisit; 
+
+	private Object phoneConfirmed;
+	
+	private Object decisionFic;
+	
+	private Object filesUpload;
+	
+	@CreatedDate
+	private Date createdAt;
+	@LastModifiedDate
+	private Date updatedAt;
+}
