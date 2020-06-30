c8d0dc0dbb4fc5fb6142de6f46e6180b9aa1c880
AnhLTD <ikuysle@gmail.com>
2020-06-30 10:02:53 +0700

    Update code

--- tpf-service-automation/src/main/java/vn.com.tpf.microservices/services/Automation/DE_MiscFrmAppDtlPage.java
+++ tpf-service-automation/src/main/java/vn.com.tpf.microservices/services/Automation/DE_MiscFrmAppDtlPage.java
@@ -150,7 +150,7 @@ public class DE_MiscFrmAppDtlPage {
     @FindBy(how = How.ID, using = "insurance_company_frmAppDtl_2")
     private WebElement companyNameElement;
 
-    @FindBy(how = How.ID, using = "amount_insurance_monthly_fee_frmAppDtl_2")
+    @FindBy(how = How.ID, using = "tpf_insurance_fee_frmAppDtl_2")
     private WebElement monthlyFeeElement;
 
     public DE_MiscFrmAppDtlPage(WebDriver driver) {
