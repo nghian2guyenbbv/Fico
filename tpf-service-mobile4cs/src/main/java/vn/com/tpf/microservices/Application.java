aa9827cd6a7bb1c6df34127b4a76f52407232edf
nghia hiep <nghiahiep512@gmail.com>
2020-07-23 10:48:11 +0700

    commit code mobile4cs

---
+++ tpf-service-mobile4cs/src/main/java/vn/com/tpf/microservices/Application.java
@@ -0,0 +1,32 @@
+package vn.com.tpf.microservices;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Value("${spring.rabbitmq.app-id}")
+    private String appId;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(appId, true, false, false);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
