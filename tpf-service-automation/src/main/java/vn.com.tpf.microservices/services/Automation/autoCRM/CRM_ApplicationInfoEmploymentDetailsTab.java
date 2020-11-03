eee99f5d4f5734316671476e9fece563aca9043a
AnhLTD <ikuysle@gmail.com>
2020-11-03 09:48:30 +0700

    Timeout Address type CRM

--- tpf-service-automation/src/main/java/vn.com.tpf.microservices/services/Automation/autoCRM/CRM_ApplicationInfoEmploymentDetailsTab.java
+++ tpf-service-automation/src/main/java/vn.com.tpf.microservices/services/Automation/autoCRM/CRM_ApplicationInfoEmploymentDetailsTab.java
@@ -2,6 +2,7 @@ package vn.com.tpf.microservices.services.Automation.autoCRM;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import jdk.jfr.Timespan;
 import lombok.Getter;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
@@ -10,6 +11,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import vn.com.tpf.microservices.models.AutoCRM.CRM_EmploymentDetailsDTO;
 import vn.com.tpf.microservices.utilities.Constant;
@@ -140,8 +142,6 @@ public class CRM_ApplicationInfoEmploymentDetailsTab {
 
     public void setData(CRM_EmploymentDetailsDTO data) throws JsonParseException, JsonMappingException, IOException {
 
-        WebDriverWait wait = new WebDriverWait(_driver, Constant.TIME_OUT_S);
-
         List<WebElement> deleteOccupationTypeElements =_driver.findElements(By.xpath("//*[contains(@id,'occupation_Info_Table')]//td[3]//ancestor::tr//*[contains(@id,'delete')]"));
 
         if (deleteOccupationTypeElements.size() > 0){
@@ -151,10 +151,11 @@ public class CRM_ApplicationInfoEmploymentDetailsTab {
             }
 
             try {
-                await("getBtnConfirmDeleteVapNextElement1 visibale Timeout!").atMost(Constant.TIME_OUT_S, TimeUnit.SECONDS)
+                await("getBtnConfirmDeleteVapNextElement1 visibale Timeout!").atMost(60, TimeUnit.SECONDS)
                         .until(() -> modalMajorChangeElement.isDisplayed());
                 Utilities.captureScreenShot(_driver);
                 btnMajorChangeElement.get(0).click();
+                Thread.sleep(15000);
             } catch (Exception e) {
                 System.out.println("Confirm Delete visibale");
             }
@@ -166,9 +167,6 @@ public class CRM_ApplicationInfoEmploymentDetailsTab {
 //        if(editOccupationTypeElements.size()>0){
 //            editOccupationTypeElements.get(0).click();
 //        }
-
-
-        wait.withMessage("occupation Type Loading timeout").until(ExpectedConditions.visibilityOf(occupationTypeElement));
 
         await("occupationTypeElement loading timeout").atMost(Constant.TIME_OUT_S, TimeUnit.SECONDS)
                 .until(() -> occupationTypeElement.isDisplayed());
