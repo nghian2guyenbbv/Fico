7452ff666c97eee2f2ec148982d889e0f27877dd
VuHH1 <vuhh1@tpb.com.vn>
2021-01-28 10:57:08 +0700

    remove allocation

--- tpf-service-autoallocation/src/main/java/vn/com/tpf/microservices/dao/ETLDataPushDAO.java
+++ tpf-service-autoallocation/src/main/java/vn/com/tpf/microservices/dao/ETLDataPushDAO.java
@@ -1,7 +1,6 @@
 package vn.com.tpf.microservices.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import vn.com.tpf.microservices.models.ETLDataPush;
 
