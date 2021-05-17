587cc645a1c1f5de7dbf66d5c9db5c7689113e9e
nghia hiep <nghiahiep512@gmail.com>
2021-05-17 11:27:23 +0700

    update cronjob: autorouting + autoallocation

---
+++ tpf-service-cronjob/src/main/java/vn/com/tpf/microservices/services/AutoRoutingCronService.java
@@ -0,0 +1,37 @@
+package vn.com.tpf.microservices.services;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+@EnableScheduling
+public class AutoRoutingCronService {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private RabbitMQService rabbitMQService;
+
+    @Scheduled(cron = "${spring.cron.autoRoutingScheduledInitCacheJob}")
+    public void job_autoRoutingScheduledInitCacheJob()
+    {
+        String slog = "func: job_autoRoutingScheduledInitCacheJob";
+        try{
+            rabbitMQService.send("tpf-service-autorouting",
+                    Map.of("func", "scheduledInitCacheJob","reference_id", UUID.randomUUID().toString()));
+        }catch (Exception e)
+        {
+            slog += " - exception: " + e.toString();
+
+        }finally {
+            log.info("{}", slog);
+        }
+    }
+}
